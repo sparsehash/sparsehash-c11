0984174d0dc179a5ecf3ecf0a85480966f5ca4cf
Donovan Hide <donovanhide@gmail.com>
2015-08-17 11:41:38 +0100

    Use static_assert

--- sparsehash/internal/hashtable-common.h
+++ sparsehash/internal/hashtable-common.h
@@ -46,11 +46,6 @@
 #include <stdexcept>  // For length_error
 
 namespace google {
-template <bool>
-struct SparsehashCompileAssert {};
-#define SPARSEHASH_COMPILE_ASSERT(expr, msg) \
-  typedef SparsehashCompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1]
-
 namespace sparsehash_internal {
 // Adaptor methods for reading/writing data from an INPUT or OUPTUT
 // variable passed to serialize() or unserialize().  For now we
@@ -158,8 +153,8 @@ bool read_bigendian_number(INPUT* fp, IntType* value, size_t length) {
   *value = 0;
   unsigned char byte;
   // We require IntType to be unsigned or else the shifting gets all screwy.
-  SPARSEHASH_COMPILE_ASSERT(static_cast<IntType>(-1) > static_cast<IntType>(0),
-                            serializing_int_requires_an_unsigned_type);
+  static_assert(static_cast<IntType>(-1) > static_cast<IntType>(0),
+                "serializing int requires an unsigned type");
   for (size_t i = 0; i < length; ++i) {
     if (!read_data(fp, &byte, sizeof(byte))) return false;
     *value |= static_cast<IntType>(byte) << ((length - 1 - i) * 8);
@@ -171,8 +166,8 @@ template <typename OUTPUT, typename IntType>
 bool write_bigendian_number(OUTPUT* fp, IntType value, size_t length) {
   unsigned char byte;
   // We require IntType to be unsigned or else the shifting gets all screwy.
-  SPARSEHASH_COMPILE_ASSERT(static_cast<IntType>(-1) > static_cast<IntType>(0),
-                            serializing_int_requires_an_unsigned_type);
+  static_assert(static_cast<IntType>(-1) > static_cast<IntType>(0),
+                "serializing int requires an unsigned type");
   for (size_t i = 0; i < length; ++i) {
     byte = (sizeof(value) <= length - 1 - i)
                ? 0
@@ -341,6 +336,4 @@ class sh_hashtable_settings : public HashFunc {
 };
 
 }  // namespace sparsehash_internal
-
-#undef SPARSEHASH_COMPILE_ASSERT
 }  // namespace google
