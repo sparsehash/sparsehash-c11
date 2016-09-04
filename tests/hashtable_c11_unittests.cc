d690c7bbc7d5bb2240ec9b3acc75eaa34afb3532
David G <david.a.grs@gmail.com>
2016-09-04 21:35:44 +0200

    dense_hash_map: adding unit test around insert r value

--- tests/hashtable_c11_unittests.cc
+++ tests/hashtable_c11_unittests.cc
@@ -9,17 +9,31 @@ using google::dense_hash_set;
 
 namespace sparsehash_internal = google::sparsehash_internal;
 
-using namespace testing;
-
-template <typename K, typename V>
-//using ht = std::unordered_map<K, V>;
-using ht = dense_hash_map<K, V>;
-
-TEST(HashtableTest, Move)
+struct HashtableMoveTest : public ::testing::Test
 {
-    using hti = ht<int, std::unique_ptr<int>>;
+    HashtableMoveTest() { h.set_empty_key(0); }
 
-    hti h;
-    typename hti::value_type p;
-    h.insert(std::move(p));
+    template <typename K, typename V>
+    using hashtable = dense_hash_map<K, V>;
+    //using hashtable = std::unordered_map<K, V>;
+
+    using ht = hashtable<int, std::unique_ptr<int>>;
+    ht h;
+};
+
+TEST_F(HashtableMoveTest, Insert_RValue)
+{
+    typename ht::value_type p1 = std::make_pair(5, std::unique_ptr<int>(new int(1234)));
+
+    auto p = h.insert(std::move(p1));
+    ASSERT_EQ(true, p.second);
+    ASSERT_EQ(5, p.first->first);
+    ASSERT_EQ(1234, *p.first->second);
+
+    p = h.insert(std::make_pair(10, std::unique_ptr<int>(new int(5678))));
+    ASSERT_EQ(true, p.second);
+    ASSERT_EQ(10, p.first->first);
+    ASSERT_EQ(5678, *p.first->second);
+
+    ASSERT_EQ(2, (int)h.size());
 }
