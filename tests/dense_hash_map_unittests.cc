6f60fb80f560b1d0023b66313212a0930f902a18
Lukas Barth <lukas.barth@kit.edu>
2018-04-17 13:55:57 +0200

    Fix emplace_hint

---
+++ tests/dense_hash_map_unittests.cc
@@ -0,0 +1,20 @@
+//
+// Created by Lukas Barth on 17.04.18.
+//
+
+#include "gtest/gtest.h"
+#include "sparsehash/dense_hash_map"
+
+using google::dense_hash_map;
+
+TEST(DenseHashMap, TestEmplaceHint) {
+	dense_hash_map<int, const char *> map;
+
+	const char * str1 = "Hello";
+
+	map.insert({42, str1});
+	auto it = map.begin();
+	map.emplace_hint(it, 1701, "World");
+
+	ASSERT_EQ(map.size(), 2);
+}
