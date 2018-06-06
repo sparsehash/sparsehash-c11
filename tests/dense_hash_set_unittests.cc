e532bf38fe8c18d5a97f8b32da678b1c62d782e7
Lukas Barth <lukas.barth@kit.edu>
2018-06-06 16:10:43 +0200

    Fix unit tests

--- tests/dense_hash_set_unittests.cc
+++ tests/dense_hash_set_unittests.cc
@@ -9,6 +9,7 @@ using google::dense_hash_set;
 
 TEST(DenseHashSet, TestEmplaceHint) {
 	dense_hash_set<const char *> set;
+	set.set_empty_key(nullptr);
 
 	const char * str1 = "Hello";
 	const char * str2 = "World";
@@ -17,5 +18,21 @@ TEST(DenseHashSet, TestEmplaceHint) {
 	auto it = set.begin();
 	set.emplace_hint(it, str2);
 
-	ASSERT_EQ(set.size(), 2);
+	ASSERT_EQ(set.size(), 2ul);
+}
+
+TEST(DenseHashSet, TestEmplaceHintAfterDelete) {
+	dense_hash_set<const char *> set;
+
+	const char * deleted_ptr = "";
+	const char * str1 = "Hello";
+
+	set.set_empty_key(nullptr);
+	set.set_deleted_key(deleted_ptr);
+
+	auto insertion_result = set.insert(str1);
+	auto str1_inserted_it = insertion_result.first;
+
+	auto deleted_iterator = set.erase(str1_inserted_it);
+	set.emplace_hint(deleted_iterator, str1);
 }
